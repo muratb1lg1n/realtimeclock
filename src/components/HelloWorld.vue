590ac2c60f99a15560b7662396f49d7dd12596e8
muratb1lg1n <68399694+muratb1lg1n@users.noreply.github.com>
2020-09-16 13:45:11 +0300

    firstcommit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,24 @@
 <template>
-  <div class="hello">
+  <div class="hello" v-if="yuk">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>{{gostergun}} {{gostertar}} {{gosteray}} {{gosteryil}}</h2>
+    <div id="app">
+      <div class="container">
+        <span id="saatler">{{gostersaat}}</span>
+      </div>
+      <div class="container">
+        <span id="ciftnokta">:</span>
+      </div>
+      <div class="container">
+        <span id="dakikalar">{{gosterdak}}</span>
+      </div>
+      <div class="container">
+        <span id="ciftnokta">:</span>
+      </div>
+      <div class="container">
+        <span id="saniyeler">{{gostersan}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,12 +27,79 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+
+  },
+  data: ()=> ({
+    gosteryil: 0,
+    gosteray: "",
+    gostertar: 0,
+    gostergun: "",
+    gostersaat: 0,
+    gosterdak: 0,
+    gostersan: 0,
+    yuk: false,
+  }),
+  methods: {
+    sifirli: girdi => (girdi < 10 ? "0" + girdi : girdi),
+    hesap(){
+      setInterval(()=>{
+         const date = new Date();
+
+         const ayarrray = ["Ocak","Subat","Mart","Nisan","Mayis","Haziran","Temmuz","Agustos","Eylul","Ekim","Kasim","Aralik"];
+         const gunarray =["Pazar","Pazartesi","Sali","Carsamba","Persembe","Cuma","Cumartesi"]
+         
+         const yil = date.getFullYear();
+         const ay = date.getMonth();
+         const tar = date.getDate();
+         const gun = date.getDay();
+         const saat = date.getHours();
+         const dakika = date.getMinutes();
+         const saniye = date.getSeconds();
+         
+         this.gosteryil = yil;
+         this.gosteray = ayarrray[ay];
+         this.gostertar = tar;
+         this.gostergun = gunarray[gun]
+         this.gostersaat = this.sifirli(saat);
+         this.gosterdak = this.sifirli(dakika);
+         this.gostersan = this.sifirli(saniye);
+
+         this.yuk = true;
+      },1000)
+    }
+  },
+  mounted() {
+    this.hesap();
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body{
+  
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+}
+#app{
+  display: flex;
+  justify-content: center;
+  padding: 10px;
+  position: relative;
+  margin-top:  5%;
+  
+}
+p {
+  font-family: "Lucida Console", Monaco, monospace;
+  padding: 10px;
+  font-size: 45px;
+  
+}
+span {
+ 
+  font-size: 95px;
+}
 h3 {
   margin: 40px 0 0;
 }
